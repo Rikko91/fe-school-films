b5be7e740f843b147c010ebcdfbd926b7c688337
omaslov <Oleg.Maslov@t-systems.com>
2019-04-30 12:23:46 +0300

    rewrite app start

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header v-if="isUserLogged"></Header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import Header from 'src/components/main/header';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		Header
+    },
+    computed: {
+		isUserLogged() {
+			return this.$store.getters.isLogged;
+		}
+	}
 }
 </script>
 
@@ -23,6 +27,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+import Vuex from 'vuex';
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+require('bootstrap/dist/js/bootstrap');
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from 'src/store/index';
+import axios from 'axios';
+import router from 'src/router/index';
+import 'nprogress/nprogress.css';
 
-Vue.config.productionTip = false
+import Vuelidate from 'vuelidate';
+Vue.use(Vuelidate);
+axios.defaults.baseURL = 'http://localhost:3000';
+
+
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
 
 new Vue({
-  render: h => h(App),
+	store,
+	router,
+	render: h => h(App),
+	beforeCreate() {
+		const user = JSON.parse(localStorage.getItem('user'));
+		if (user) {
+			this.$store.dispatch('LOGIN', user)
+		}
+	}
 }).$mount('#app')
