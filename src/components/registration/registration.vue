fb933f221df0d35f21ebc40420773957d0ad556b
omaslov <Oleg.Maslov@t-systems.com>
2019-05-06 02:59:48 +0300

    validation in registration components

--- src/components/registration/registration.vue
+++ src/components/registration/registration.vue
@@ -7,9 +7,9 @@
 					<div class="form-group">
 						<label for="email">Email address</label>
 						<input type="text" v-model="user.email" name="email" class="form-control mx-auto" placeholder="Enter email" required/>
-						<span class="error" v-if="$v.user.email.$invalid">
-                            Error. This field should have a correct email address!
-                        </span>
+						<b-alert class="mt-2" variant="danger" :show="$v.user.email.$invalid">
+                            This field should have a correct email address!
+                        </b-alert>
 					</div>
 					<div class="form-group">
 						<label for="username">Your name</label>
@@ -22,25 +22,36 @@
 					<div class="form-group">
 						<label for="password">Your password</label>
 						<input type="password" v-model="user.password" name="password" class="form-control mx-auto" placeholder="Enter password">
+						<b-alert class="mt-2" variant="danger" :show="$v.user.password.$invalid">
+							Password must have at least {{ $v.user.password.$params.minLength.min }} letters.
+						</b-alert>
 					</div>
 					<div class="form-group">
 						<label for="passwordRepeated">Repeat password please</label>
-						<input type="password" v-model="passwordRepeated" name="passwordRepeated" class="form-control mx-auto" placeholder="Enter password one more">
+						<input type="password" v-model="user.passwordRepeated" name="passwordRepeated" class="form-control mx-auto" placeholder="Enter password one more">
+						<b-alert class="mt-2" variant="danger" :show="$v.user.passwordRepeated.$invalid">
+							Passwords must be identical
+						</b-alert>
 					</div>
+					<b-alert
+						variant="danger"
+						fade
+						:show="showAlert">
+						{{ alertMessage }}
+					</b-alert>
 					<div clas="form-group">
-						<button type="submit" class="btn btn-outline-light mb-2">Register</button>
+						<button type="submit" class="btn btn-outline-light mb-2" :disabled="$v.$invalid">Register</button>
 						<router-link to="/login" class="btn btn-link">Go back to Login page</router-link>
 					</div>
 				</form>
 			</div>
 		</div>
-
 	</div>
 </template>
 
 <script>
 	import {register as registerUser, getUserByEmail} from "src/services/user/user.service";
-	import {email} from 'vuelidate/lib/validators';
+	import {required, email, minLength, sameAs} from 'vuelidate/lib/validators';
 	export default {
 		name: "Registration",
 		components: {
@@ -52,27 +63,53 @@
 					name: '',
 					surname: '',
 					password: '',
+					passwordRepeated: '',
 				},
-				passwordRepeated: ''
+				showAlert: false,
+				alertMessage: ''
 			}
 		},
 		methods: {
 			register() {
 				getUserByEmail(this.user.email).then(existingUsers => {
 					if (existingUsers.length === 0) {
-						registerUser(this.user).then(() => {
-							this.$router.push({ name: 'login'})
+						registerUser({
+							email: this.user.email,
+							name: this.user.name,
+							surname: this.user.surname,
+							password: this.user.password
+						}).then(() => {
+							this.showToast('You have successfully registered and will be redirected to the login page in 5 seconds');
+							setTimeout(() => this.$router.push({ name: 'login'}), 5000);
 						});
+					} else {
+						this.alertMessage = 'Users with this email already exists! Please change an email address';
+						this.showAlert = true;
 					}
+				});
+			},
+			showToast(message) {
+				this.$bvToast.toast(message, {
+					title: 'Information',
+					autoHideDelay: 5000,
+					variant: 'info'
 				});
 			}
 		},
 		validations: {
 			user: {
 				email: {
+					required,
 					email
+				},
+				password: {
+					required,
+					minLength: minLength(6)
+				},
+				passwordRepeated: {
+					sameAsPassword: sameAs('password')
 				}
-			}
+			},
 		}
 	}
 </script>
