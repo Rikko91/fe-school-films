13e6589662634a220b1af4db9f0093fff4dcef20
omaslov <Oleg.Maslov@t-systems.com>
2019-05-06 03:00:30 +0300

    add comments

--- src/components/comments/comments.vue
+++ src/components/comments/comments.vue
@@ -43,26 +43,27 @@
 											class="form-control comment-text-area"
 											placeholder="Leave your comment"
 											rows="4"
+											v-model="textComment"
 										></textarea>
 									</div>
 								</div>
 							</div>
-							<div class="row">
-								<div class="col-2">
-									Attach video
-								</div>
-								<div class="col-10">
-									<div class="input-group mb-3">
-										<div class="input-group-prepend">
-											<span class="input-group-text">http://</span>
-										</div>
-										<input type="text" class="form-control">
-									</div>
-								</div>
-							</div>
+							<!--<div class="row">-->
+								<!--<div class="col-2">-->
+									<!--Attach video-->
+								<!--</div>-->
+								<!--<div class="col-10">-->
+									<!--<div class="input-group mb-3">-->
+										<!--<div class="input-group-prepend">-->
+											<!--<span class="input-group-text">http://</span>-->
+										<!--</div>-->
+										<!--<input type="text" class="form-control" v-model="videoUrl">-->
+									<!--</div>-->
+								<!--</div>-->
+							<!--</div>-->
 							<div class="row">
 								<div class="col">
-									<button class="btn btn-info text-uppercase ">Send message</button>
+									<button class="btn btn-info text-uppercase" @click="addComment" :disabled="!textComment">Add new comment</button>
 								</div>
 							</div>
 						</div>
@@ -76,6 +77,8 @@
 <script>
 	import {getComments} from "../../services/film/filmService";
 	import oneComment from 'src/components/comments/oneComment';
+	import dayjs from 'dayjs';
+	import {addNewComment} from "src/services/film/filmService";
 	export default {
 		name: "comments",
 		components: {
@@ -83,19 +86,44 @@
 		},
 		computed: {
 			filmId() {
-				return this.$store.getters.getLastSelectedFilm;
+				return this.$store.getters.lastSelectedFilm.id;
+			},
+			userId() {
+				return this.$store.getters.user.id;
+			}
+		},
+		watch: {
+			filmId(value) {
+				getComments(value).then(comments => {
+					this.comments = comments;
+				})
 			}
 		},
 		data() {
 			return {
-				comments: []
+				comments: [],
+				textComment: '',
 			}
 		},
-		created() {
-			getComments(1).then(comments => {
+		methods: {
+			addComment() {
+				let comment = {
+					userId: this.userId,
+					filmId: this.filmId,
+					message: this.textComment,
+					likes: '0',
+					dislikes: '0',
+					created: dayjs().format('YYYY-MM-DD')
+				};
+
+				addNewComment(comment);
+			}
+		},
+		mounted() {
+			getComments(this.filmId).then(comments => {
 				this.comments = comments;
 			})
-		}
+		},
 	}
 </script>
 
